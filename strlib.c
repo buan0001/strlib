f706c129ed6a48699fe36bb06883f7bf2c1a5020
buan0001 <bustermikkel@gmail.com>
2024-09-20 12:07:07 +0200

    Commented all function to be added

---
+++ strlib.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    return 0;
+}
+
+char at(int index){
+    return 'c';
+}
+
+/* 
+at()
+charAt()
+endsWith()
+includes()
+indexOf()
+lastIndexOf()
+localeCompare()
+padEnd()
+padStart()
+repeat()
+slice()
+startsWith()
+substring()
+toLocaleLowerCase() <- wtf?
+toLocaleUpperCase() <- wtf?
+toString()
+toUpperCase()
+toLowerCase()
+trim()
+trimEnd()
+trimStart()
+valueOf()
+*/
+
+/* REGEX METHODS:
+match()
+mastchAll()
+replace()
+replaceAll()
+search()
+split()
+*/
